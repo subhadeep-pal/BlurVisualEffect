75393ed327f6bb303daa64267aeb2d3a426a3943
Subhadeep Pal <subhadeep.pal@tcs.com>
2017-04-24 15:45:19 +0530

    Initial Commit

---
+++ BlurEffect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlurEffect
+//
+//  Created by 01HW934413 on 24/04/17.
+//  Copyright © 2017 01HW934413. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
